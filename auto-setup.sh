ff37fbb66f25b8e762588e057474f01a5f6fd71c
Ivo Berger <ivo-berger@outlook.com>
2016-11-13 00:33:20 +0100

    go setup added

--- auto-setup.sh
+++ auto-setup.sh
@@ -5,15 +5,30 @@
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable -y
 sudo add-apt-repository ppa:dlech/keepass2-plugins -y
-
-# install packages
 sudo apt update
-# tools
+
+# install tools
 sudo apt install keepass2 keepass2-plugin-keepasshttp keepass2-plugin-keeagent keepass2-plugin-application-indicator -y
-# development
+sudo apt install grub-customizer -y
+# set up development environment
 sudo apt install git cmake golang -y
+    #golang Setup
+    mkdir $HOME/Documents/go
+    export GOPATH=$HOME/Documents/go
+    mkdir $GOPATH/bin
+    mkdir $GOPATH/src
+    mkdir $GOPATH/lib
+    mkdir -p $GOPATH/src/github.com/iiiuminator
+    mkdir -p $GOPATH/src/gitlab.com/illuminator
+    mkdir -p $GOPATH/src/bitbucket.com/illuminatos
+    mkdir -p $GOPATH/src/git.sit.fraunhofer.de/iberger
+
+    cp /etc/skel/.profile $HOME/.profile
+    echo 'export GOPATH=$HOME/Documents/go' >> $HOME/.profile
+    echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.profile
+
 # media
-sudo apt install vlc* -y
+sudo apt install vlc* owncloud-client -y
 
 # upgrade packages
 sudo apt upgrade -y
