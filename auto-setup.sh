7c7d696f074efe2fd1788e3cf60d298616619564
Ivo <IIIuminator@users.noreply.github.com>
2016-11-13 00:30:33 +0100

    added atom, oracle java, y ppa manger and tor browser

--- auto-setup.sh
+++ auto-setup.sh
@@ -5,13 +5,17 @@
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable -y
 sudo add-apt-repository ppa:dlech/keepass2-plugins -y
+sudo add-apt-repository ppa:webupd8team/atom -y
+sudo add-apt-repository ppa:webupd8team/java -y
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager -y
+sudo add-apt-repository ppa:webupd8team/tor-browser -y
 
 # install packages
 sudo apt update
 # tools
 sudo apt install keepass2 keepass2-plugin-keepasshttp keepass2-plugin-keeagent keepass2-plugin-application-indicator -y
 # development
-sudo apt install git cmake golang -y
+sudo apt install git cmake golang atom -y
 # media
 sudo apt install vlc* -y
 
