c2245c4e245d984c2d0e0fad7e469861171f8a01
Ivo Berger <ivo-berger@outlook.com>
2017-03-24 21:18:10 +0100

    made executable

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,2 +1,2 @@
-sudo pacman -S --noconfirm texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools &
+sudo pacman -S --noconfirm texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility &
 yaourt -S --noconfirm google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer
