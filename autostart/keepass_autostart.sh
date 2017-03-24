94177de6ace60da9e282d1c623ef9421f805537b
Ivo Berger <ivo-berger@outlook.com>
2017-03-24 22:15:53 +0100

    added gnome keyring

--- autostart/keepass_autostart.sh
+++ autostart/keepass_autostart.sh
@@ -2,4 +2,4 @@
 # startup keepass with a password from KWallet
 walletkey=$(kwallet-query kdewallet -r KeePass)
 dbpath="/run/media/ivo/Data_Main/Nextcloud/Software/KeePassPortable/MainDB.kdbx"
-echo "$walletkey" | keepass2 $dbpath --pw-stdin -minimize
+echo "$walletkey" | keepass $dbpath --pw-stdin -minimize
