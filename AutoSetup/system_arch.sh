22ca5c3eef5f78556c65143c4c1059aef616fccd
Ivo Berger <ivo-berger@outlook.com>
2017-03-24 21:12:37 +0100

    added arch script

---
+++ AutoSetup/system_arch.sh
@@ -0,0 +1,2 @@
+sudo pacman -S --noconfirm texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools &
+yaourt -S --noconfirm google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer
