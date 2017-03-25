51f7c1dec65cf4f01093641553912080f13ab7fb
Ivo Berger <ivo-berger@outlook.com>
2017-03-25 20:08:46 +0100

    added libgnome

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,2 +1,5 @@
-sudo pacman -S --noconfirm texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring &
+yaourt -Syu --aur --noconfirm
+sudo pacman -S --noconfirm texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring linphone
 yaourt -S --noconfirm google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo
+sudo pacman -Rns $(sudo pacman -Qdtq) --noconfirm
+sudo pacman -Sc --noconfirm
