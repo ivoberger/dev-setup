fc336de4e48286fa5404ff99354b4f1ee02878ac
Ivo Berger <ivo-berger@outlook.com>
2017-03-28 14:51:49 +0200

    added bash-completion

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,4 +1,4 @@
 yaourt -Syua --noconfirm
-sudo pacman -S --noconfirm --needed texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring redshift
+sudo pacman -S --noconfirm --needed texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring redshift bash-completion
 yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo skypeforlinux-bin
 yaourt -Qdt --noconfirm
