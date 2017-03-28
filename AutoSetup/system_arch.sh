9459ec389238440008f0955e77a0d6d74746b933
Ivo Berger <ivo-berger@outlook.com>
2017-03-28 14:05:42 +0200

    removed ring

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,4 +1,4 @@
 yaourt -Syua --noconfirm
 sudo pacman -S --noconfirm --needed texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring redshift
-yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo ring-kde skypeforlinux-bin
+yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo skypeforlinux-bin
 yaourt -Qdt --noconfirm
