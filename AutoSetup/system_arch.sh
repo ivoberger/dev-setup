1e4276a2de2d9e3ac068845cd03d6c9d7e700cfe
Ivo Berger <ivo-berger@outlook.com>
2017-03-30 17:54:44 +0200

    added gitkraken

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,4 +1,10 @@
+<<<<<<< Updated upstream
 yaourt -Syyua --noconfirm
 sudo pacman -S --noconfirm --needed texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring redshift bash-completion gutenprint
 yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo skypeforlinux-bin
+=======
+yaourt -Syua --noconfirm
+sudo pacman -S --noconfirm --needed texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring redshift bash-completion
+yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo skypeforlinux-bin gitkraken
+>>>>>>> Stashed changes
 yaourt -Qdt --noconfirm
