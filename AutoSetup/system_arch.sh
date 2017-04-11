369ac567d7737c7b1ea2eb98291cddee89b267a4
Ivo Berger <ivo-berger@outlook.com>
2017-04-11 14:09:11 +0200

    added gputest and prime95

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,4 +1,4 @@
 yaourt -Syyua --noconfirm
 sudo pacman -S --noconfirm --needed texmaker texlive-most keepass keepass-plugin-keeagent evolution evolution-ews gimp scribus synergy go-tools gnome-disk-utility gnome-keyring libgnome-keyring redshift bash-completion gutenprint filezilla
-yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo skypeforlinux-bin gitkraken
+yaourt -S --noconfirm --needed google-chrome intellij-idea-ultimate-edition keepass-plugin-http telegram-desktop-bin keebuntu gogland jdk visual-studio-code nextcloud-client grub-customizer gigolo skypeforlinux-bin gitkraken gputest mprime
 yaourt -Qdt --noconfirm
