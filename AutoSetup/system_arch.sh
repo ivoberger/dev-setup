5a0a58774bd71d5d28bef94c6fd2c0f3e19b0872
Ivo Berger <ivo-berger@outlook.com>
2017-04-21 15:51:44 +0200

    added python2 pip

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,11 +1,10 @@
-# System Update
 yaourt -Syyua --noconfirm  
 # Internet
 yaourt -S --noconfirm --needed google-chrome evolution evolution-ews telegram-desktop-bin nextcloud-client skypeforlinux-bin
 # Office
 yaourt -S --noconfirm --needed texmaker texlive-most gimp scribus
 # Development
-yaourt -S --noconfirm --needed intellij-idea-ultimate-edition pycharm-professional gogland visual-studio-code jdk go-tools gitkraken filezilla
+yaourt -S --noconfirm --needed intellij-idea-ultimate-edition pycharm-professional gogland visual-studio-code jdk go-tools gitkraken filezilla python2-pip
 # Tools
 yaourt -S --noconfirm --needed keepass keepass-plugin-keeagent keepass-plugin-http keebuntu grub-customizer gigolo gputest mprime gnome-disk-utility synergy 
 # Miscelanues
