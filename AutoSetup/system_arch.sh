e50718af866fa3e058135dc30d9725f35ceee522
Ivo Berger <ivo-berger@outlook.com>
2017-07-18 13:54:27 +0200

    system arch setup updatet

--- AutoSetup/system_arch.sh
+++ AutoSetup/system_arch.sh
@@ -1,13 +1,16 @@
 yaourt -Syyua --noconfirm  
 # Internet
-yaourt -S --noconfirm --needed google-chrome evolution evolution-ews telegram-desktop-bin nextcloud-client skypeforlinux-bin
+yaourt -S --noconfirm google-chrome evolution evolution-ews telegram-desktop-bin nextcloud-client skypeforlinux-bin
 # Office
-yaourt -S --noconfirm --needed texmaker texlive-most gimp scribus
+yaourt -S --noconfirm texmaker texlive-most gimp scribus
 # Development
-yaourt -S --noconfirm --needed intellij-idea-ultimate-edition pycharm-professional gogland visual-studio-code jdk go-tools gitkraken filezilla python2-pip
+yaourt -S --noconfirm intellij-idea-ultimate-edition pycharm-professional gogland visual-studio-code jdk go-tools gitkraken filezilla python2-pip android-studio
 # Tools
-yaourt -S --noconfirm --needed keepass keepass-plugin-keeagent keepass-plugin-http keebuntu grub-customizer gigolo gputest mprime gnome-disk-utility synergy 
+yaourt -S --noconfirm keepass keepass-plugin-keeagent keepass-plugin-http keebuntu grub-customizer gigolo gnome-disk-utility synergy 
 # Miscelanues
-yaourt -S --noconfirm --needed gutenprint redshift bash-completion gnome-keyring libgnome-keyring 
+yaourt -S --noconfirm gutenprint redshift bash-completion gnome-keyring libgnome-keyring 
 # Cleanup
 yaourt -Qdt
+
+# autostarts
+ln -s ../autostart/keepass_autostart.sh ~/.config/autostart-scripts/keepass_autostart.sh
