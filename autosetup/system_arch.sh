79eb43da6896e75128188d7eb65a04901b461894
Ivo Berger <ivo-berger@outlook.com>
2017-08-06 16:46:08 +0200

    parallelism

--- autosetup/system_arch.sh
+++ autosetup/system_arch.sh
@@ -4,20 +4,18 @@ sudo pacman -Rsn --noconfirm konversation jre8-openjdk jdk8-openjdk kwrite samba
 # Full system upgrade
 sudo pacman -Syyu --noconfirm
 # System
-sudo pacman -S --noconfirm --needed linux49 linux49-nvidia linux49-r8168 linux412 linux412-nvidia linux412-r8168
+sudo konsole -e pacman -S --noconfirm --needed linux49 linux49-nvidia linux49-r8168 linux412 linux412-nvidia linux412-r8168
 # Internet
-yaourt -S --noconfirm --needed google-chrome telegram-desktop-bin nextcloud-client skypeforlinux-bin &
+konsole -e yaourt -S --noconfirm --needed google-chrome telegram-desktop-bin nextcloud-client skypeforlinux-bin
 # Office
-yaourt -S --noconfirm --needed texmaker texlive-most gimp scribus okular &
+konsole -e yaourt -S --noconfirm --needed texmaker texlive-most gimp scribus okular
 # Tools
-yaourt -S --noconfirm --needed keepass keepass-plugin-keeagent keepass-plugin-http keebuntu grub-customizer gigolo gnome-disk-utility synergy xnviewmp unzip &
+konsole -e yaourt -S --noconfirm --needed keepass keepass-plugin-keeagent keepass-plugin-http keebuntu grub-customizer gigolo gnome-disk-utility synergy xnviewmp unzip
 # Miscellaneous
-yaourt -S --noconfirm --needed redshift bash-completion gpmdp &
+konsole -e yaourt -S --noconfirm --needed redshift bash-completion gpmdp
 # Development
-yaourt -S --noconfirm --needed jdk go-tools python2-pip python-pip &
-yaourt -S --noconfirm --needed intellij-idea-ultimate-edition pycharm-professional gogland android-studio atom
-# Cleanup
-yaourt -Qdt
+konsole -e yaourt -S --noconfirm --needed jdk go-tools python2-pip python-pip
+konsole -e yaourt -S --noconfirm --needed intellij-idea-ultimate-edition pycharm-professional gogland android-studio atom
 
-bash keepassXhttp.sh
+konsole -e bash keepassXhttp.sh
 yaourt -Qdt
