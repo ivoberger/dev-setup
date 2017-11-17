240599cb16ada249aee6d8a78b34807f98828a57
Ivo <ivo-berger@outlook.com>
2017-11-17 14:00:12 +0100

    moved pkgs to snap and ubuntu-make

--- autosetup/system_debian_based.sh
+++ autosetup/system_debian_based.sh
@@ -4,39 +4,23 @@
 # add Repositories
 sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
 sudo add-apt-repository ppa:ubuntu-lxc/lxd-stable -y
-sudo add-apt-repository ppa:dlech/keepass2-plugins -y
-sudo add-apt-repository ppa:webupd8team/atom -y
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo add-apt-repository ppa:nilarimogard/webupd8 -y
 sudo add-apt-repository ppa:nextcloud-devs/client -y
-sudo add-apt-repository ppa:atareao/telegram -y
-sudo add-apt-repository ppa:maarten-fonville/android-studio -y
-sudo add-apt-repository ppa:mmk2410/intellij-idea -y
-sudo add-apt-repository ppa:longsleep/golang-backports -y
 sudo add-apt-repository ppa:mozillateam/firefox-next -y
-
-sudo add-apt-repository ppa:viktor-krivak/pycharm -y
-sudo sh -c 'echo deb http://ppa.launchpad.net/viktor-krivak/pycharm/ubuntu zesty main > /etc/apt/sources.list.d/viktor-krivak-ubuntu-pycharm-xenial.list'
 
 sudo sh -c 'echo "deb http://archive.getdeb.net/ubuntu $(lsb_release -sc)-getdeb apps" > /etc/apt/sources.list.d/getdeb.list'
 wget -q -O - http://archive.getdeb.net/getdeb-archive.key | sudo apt-key add -
 
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
-
-wget -qO - https://gpmdp.xyz/bintray-public.key.asc | sudo apt-key add -
-sudo sh -c 'echo "deb https://dl.bintray.com/marshallofsound/deb debian main" >> /etc/apt/sources.list.d/gpmdp.list'
-
 sudo apt update
 
-sudo apt install git cmake golang atom oracle-java9-installer oracle-java9-set-default -y
+sudo apt install git cmake oracle-java8-installer oracle-java8-set-default -y
 
 # install tools
 sudo apt install keepassx -y
-sudo apt install grub-customizer texlive-full texmaker thunderbird libreoffice yakuake -y
+sudo apt install vlc grub-customizer texlive-full texmaker evolution evolution-ews libreoffice yakuake nextcloud-client-nautilus -y
 
 # set up development environment
-sudo apt install android-studio intellij-idea-ultimate pycharm-professional -y
     #golang Setup
     mkdir $HOME/go
     export GOPATH=$HOMEgo
@@ -48,12 +32,6 @@ sudo apt install android-studio intellij-idea-ultimate pycharm-professional -y
     echo 'export GOPATH=$HOME/go' >> $HOME/.profile
     echo 'export PATH=$PATH:$GOPATH/bin' >> $HOME/.profile
 
-# media
-sudo apt install vlc* redshift gimp google-play-music-desktop-player -y
-
-# internet
-sudo apt install nextcloud-client google-chrome-stable telegram -y
-
 cd /tmp
 wget https://binaries.symless.com/v2.0.1/synergy_2.0.1.stable-b1034%2B59dd93a0_amd64.deb
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
@@ -61,6 +39,16 @@ wget https://desktop-download.mendeley.com/download/apt/pool/main/m/mendeleydesk
 wget https://github.com/downloads/maoserr/redshiftgui/RedshiftGUI-0.2.1-Linux-x86_64.deb
 sudo dpkg -i *.deb
 rm *.deb
+
+sudo snap install intellij-idea-ultimate --classic
+sudo snap install pycharm-professional --classic
+sudo snap install atom --classic
+sudo snap install ubuntu-make --classic
+sudo snap install telegram-latest
+
+ubuntu-make.umake ide eclipse
+ubuntu-make.umake ide gogland --eap
+ubuntu-make.umake android android-studio
 
 # gdr
 chmod +x TU/gdr_aptonly.sh
