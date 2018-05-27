e589405bd8674d1130105c4e82ea6f4453c8599f
Ivo <ivo-berger@outlook.com>
2018-05-27 17:11:35 +0300

    fixex TUD CD script

--- autosetup/TU/tud_cd.sh
+++ autosetup/TU/tud_cd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 updmap
 cd "$1"
-cp -R texmf/* $HOME/.texmf-var/
+cp -R texmf/* $HOME/.texlive2017/texmf-var/
 sudo texhash
-updmap --enable Map 5ch.map
-updmap --enable Map 5fp.map
-updmap --enable Map 5sf.map
-updmap --listmaps | egrep "^Map[[:blank:]]*5"
+updmap -user --enable Map 5ch.map
+updmap -user --enable Map 5fp.map
+updmap -user --enable Map 5sf.map
+updmap -user --listmaps | egrep "^Map[[:blank:]]*5"
