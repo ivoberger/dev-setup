b45d22ec6d0c58adeb79614cf36c70657e78cab5
Ivo Berger <ivo@ivoberger.com>
2020-04-12 19:52:09 +0200

    set zsh as default shell in wsl autosetup

--- autosetup/wsl_ubuntu.sh
+++ autosetup/wsl_ubuntu.sh
@@ -3,6 +3,7 @@ sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install rcm zsh curl
+sudo chsh --shell=/usr/bin/zsh ivo
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 rcup -d dotfiles -f
 zsh -c "cd ~; source .zshrc; zimfw install"
