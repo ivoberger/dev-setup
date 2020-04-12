2446977c0284a2749117d52f6ccb107b2632d59d
Ivo Berger <ivo@ivoberger.com>
2020-04-12 20:03:46 +0200

    cleanup

--- autosetup/TU/tud_cd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-updmap
-cd "$1"
-cp -R texmf/* $HOME/.texlive2017/texmf-var/
-sudo texhash
-updmap -user --enable Map 5ch.map
-updmap -user --enable Map 5fp.map
-updmap -user --enable Map 5sf.map
-updmap -user --listmaps | egrep "^Map[[:blank:]]*5"
