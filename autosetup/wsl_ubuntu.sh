821ddb796bcac8143c08141592da6b402db7b1d5
Ivo Berger <ivo@ivoberger.com>
2020-04-13 12:59:23 +0200

    add build-essential to wsl setup

--- autosetup/wsl_ubuntu.sh
+++ autosetup/wsl_ubuntu.sh
@@ -2,7 +2,7 @@
 sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install rcm zsh curl
+sudo apt -y install rcm zsh curl build-essential
 sudo chsh --shell=/usr/bin/zsh ivo
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 rcup -d dotfiles -f
