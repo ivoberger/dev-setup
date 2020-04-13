45e445b0aad46a84beef4f809f6ae565e11af577
Ivo Berger <ivo@ivoberger.com>
2020-04-13 15:56:18 +0200

    add command_not_found functionality to zsh setup

--- autosetup/wsl_ubuntu.sh
+++ autosetup/wsl_ubuntu.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository -y https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install rcm zsh curl build-essential adoptopenjdk-14-openj9
+sudo apt -y install rcm zsh curl build-essential adoptopenjdk-14-openj9 command-not-found
 sudo chsh --shell=/usr/bin/zsh ivo
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 rcup -d dotfiles -f
