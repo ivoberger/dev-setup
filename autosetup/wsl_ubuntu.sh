2144db99bf190e4ea69ea6d281007dc5a7d97df3
Ivo Berger <ivo@ivoberger.com>
2020-04-13 13:12:13 +0200

    add adoptopenjdk-14-openj9 to wsl setup

--- autosetup/wsl_ubuntu.sh
+++ autosetup/wsl_ubuntu.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
+sudo add-apt-repository -y https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install rcm zsh curl build-essential
+sudo apt -y install rcm zsh curl build-essential adoptopenjdk-14-openj9
 sudo chsh --shell=/usr/bin/zsh ivo
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 rcup -d dotfiles -f
