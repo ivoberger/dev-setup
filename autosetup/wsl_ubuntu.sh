377a5437b02e58f948ca2dc72440b6372dc0d529
Ivo Berger <ivo@ivoberger.com>
2020-04-13 17:15:13 +0200

    add flutter deps to zsh stup

--- autosetup/wsl_ubuntu.sh
+++ autosetup/wsl_ubuntu.sh
@@ -4,7 +4,8 @@ sudo add-apt-repository -y https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 sudo apt update
 sudo apt -y upgrade
-sudo apt -y install rcm zsh curl build-essential adoptopenjdk-14-openj9 command-not-found
+sudo apt -y install rcm zsh curl build-essential adoptopenjdk-14-openj9 command-not-found unzip
+sudo apt -y install curl unzip zip libglu1-mesa xz-utils
 sudo chsh --shell=/usr/bin/zsh ivo
 curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
 rcup -d dotfiles -f
