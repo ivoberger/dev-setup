8beae526516363069040db10ef6a1cae63282a94
Ivo Berger <ivo.berger@main-incubator.com>
2021-01-18 08:34:58 +0100

    add yarn bin to PATH, remove old PPA

--- autosetup/wsl_ubuntu.sh
+++ autosetup/wsl_ubuntu.sh
@@ -7,7 +7,6 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyr
 wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo add-apt-repository -y https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
-sudo add-apt-repository -y ppa:martin-frost/thoughtbot-rcm
 sudo apt update
 sudo apt -y install -f
 sudo apt -y upgrade
