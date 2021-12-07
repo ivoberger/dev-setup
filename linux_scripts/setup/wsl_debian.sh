ec29e6d467efd4585efc5eae5b5197ec135b1404
Ivo Berger <ivo@ivoberger.com>
2021-12-07 16:46:36 +0100

    fix: wsl setup

--- linux_scripts/setup/wsl_debian.sh
+++ linux_scripts/setup/wsl_debian.sh
@@ -4,19 +4,17 @@ sudo apt install -y wget curl git unzip zip zsh xz-utils libgbm-dev gnupg softwa
 sudo apt-file update
 
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo dpkg -i ./gitkraken-amd64.deb
+sudo apt install -y ./gitkraken-amd64.deb
 rm ./gitkraken-amd64.deb
 
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-wget -qO - https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public | sudo apt-key add -
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo add-apt-repository -y https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 sudo apt update
 sudo apt -y install -f
 sudo apt -y upgrade
-sudo apt -y install rcm build-essential adoptopenjdk-14-openj9
-sudo apt -y install libglu1-mesa google-cloud-sdk libxss1 libasound2
+sudo apt -y install rcm build-essential
+sudo apt -y install google-cloud-sdk
 sudo chsh --shell=/usr/bin/zsh ivo
-curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+
 rcup -d dotfiles -f
-zsh -c "cd ~; source .zshrc; zimfw install"
+zsh; exit;
