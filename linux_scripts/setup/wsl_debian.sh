decbd81af171d3a18d410e1c141f83b715105a57
Ivo <ivo@gigpro.com>
2022-05-20 20:47:11 +0200

    remove gitkraken

--- linux_scripts/setup/wsl_debian.sh
+++ linux_scripts/setup/wsl_debian.sh
@@ -3,10 +3,6 @@ sudo apt update
 sudo apt install -y wget curl git unzip zip zsh xz-utils libgbm-dev gnupg software-properties-common command-not-found socat
 sudo apt-file update
 
-wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
-sudo apt install -y ./gitkraken-amd64.deb
-rm ./gitkraken-amd64.deb
-
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 sudo apt update
