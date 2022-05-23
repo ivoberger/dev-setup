c199410f648a788e3f77263a2ec649dcdbcb3647
Ivo Berger <ivo@ivoberger.com>
2022-05-23 11:37:17 +0200

    fix

--- linux_scripts/setup/wsl_debian.sh
+++ linux_scripts/setup/wsl_debian.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt-get -yqq update
+sudo apt-get update
 sudo apt-get install -yqq wget curl git unzip zip zsh xz-utils gnupg software-properties-common command-not-found socat apt-transport-https ca-certificates
 sudo apt-file update
 
 sudo apt-get update
 sudo apt-get -yqq install -f
-sudo apt-get -yqq upgrade
+yes | sudo DEBIAN_FRONTEND=noninteractive apt-get -yqq upgrade
 sudo apt-get -yqq install rcm build-essential
 sudo chsh --shell=/usr/bin/zsh $USER
 
