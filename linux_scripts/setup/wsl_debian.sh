588eda3e6e4ce8f6174e7c2508bb6fe99c4c302f
Ivo Berger <ivo@ivoberger.com>
2022-05-23 11:30:33 +0200

    remove gcloud sdk repo

--- linux_scripts/setup/wsl_debian.sh
+++ linux_scripts/setup/wsl_debian.sh
@@ -3,8 +3,6 @@ sudo apt-get update
 sudo apt-get install -yqq wget curl git unzip zip zsh xz-utils gnupg software-properties-common command-not-found socat apt-transport-https ca-certificates
 sudo apt-file update
 
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
 sudo apt-get update
 sudo apt-get -yqq install -f
 sudo apt-get -yqq upgrade
