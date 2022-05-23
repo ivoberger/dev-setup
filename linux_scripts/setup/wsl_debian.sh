510d58ebd0b4063763f8f2d3b46507ed7b1515ed
Ivo Berger <ivo@ivoberger.com>
2022-05-23 11:29:20 +0200

    fix: codepaces setup

--- linux_scripts/setup/wsl_debian.sh
+++ linux_scripts/setup/wsl_debian.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-sudo apt update
-sudo apt install -y wget curl git unzip zip zsh xz-utils gnupg software-properties-common command-not-found socat
+sudo apt-get update
+sudo apt-get install -yqq wget curl git unzip zip zsh xz-utils gnupg software-properties-common command-not-found socat apt-transport-https ca-certificates
 sudo apt-file update
 
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-sudo apt update
-sudo apt -y install -f
-sudo apt -y upgrade
-sudo apt -y install rcm build-essential
+sudo apt-get update
+sudo apt-get -yqq install -f
+sudo apt-get -yqq upgrade
+sudo apt-get -yqq install rcm build-essential
 sudo chsh --shell=/usr/bin/zsh $USER
 
 rcup -d dotfiles -f
+zsh -c "nvm install --lts"
+zsh -c "npm i -g npm pnpm yarn"
 zsh; exit;
